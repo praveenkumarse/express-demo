1681ab0d7bb8c7ce81842f8e4f4937be85928c44
Praveen Kumar <praveenkumar@innotical.com>
2017-07-12 18:50:29 +0530

    push

--- controller/home.js
+++ controller/home.js
@@ -2,33 +2,49 @@ var userInfo = require("./../model/home");
 
 module.exports.controller = function(app) {
     app.get('/info', function(req, res, next) {
-        userInfo.find({}, function(err, data) {
-            res.send(data[0]);
+        let info;
+        userInfo.find({}).then(function(data) {
+            res.json({
+                info: data[0].info
+            });
+        }, err => {
+
         })
     });
     app.post('/add', function(req, res, next) {
-        var info = new userInfo({
-            info: req.body.info
-        })
-        info.save(function(err) {
-            if (err) throw err;
-            res.send("add data successfully")
-        })
-    });
-    app.delete('/delete/:id', function(req, res, next) {
 
-        userInfo.findByIdAndUpdate({
-            $pull: {
-                events: {
-                    _id: req.params.id
-                }
-            }
-        }, function(err) {
-            if (err) throw err;
-            else res.send("deleted")
+        var query = {},
+            update = { expire: new Date() },
+            options = { upsert: true, new: true, setDefaultsOnInsert: true };
+        userInfo.findOneAndUpdate(query, update, options, function(error, result) {
+            if (error) return;
+            result.info = req.body.info;
+            result.save(function(err) {
+                if (err) throw err;
+                res.json("add data successfully")
+            })
         });
     });
-    app.put('/update/:id', function(req, res, next) {
-
+    app.delete('/delete/:id', function(req, res, next) {
+        userInfo.findOneAndUpdate({}, { $pull: { info: { _id: req.params.id } } },
+            function(err, data) {
+                if (err) return err;
+                res.json("Data delete successfully")
+            });
     });
+    app.put('/update/:id', function(req, res, next) {
+        userInfo.find({}, { info: { $elemMatch: { _id: req.params.id } } },
+            function(error, data) {
+                userInfo.update({ 'data._id': req.params.id }, {
+                    '$set': {
+                        'data.$.firstName': req.body.firstName,
+                        'data.$.lastName': req.body.firstName,
+                        'data.$.number': req.body.number,
+                    }
+                }, function(err) {
+                    if (err) return err;
+                    res.json("Update Data Successfully")
+                });
+            })
+    })
 }
